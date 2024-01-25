eb5dd3fc1c959a50fe0ccae9528239fd458a8f2e
猫猫摸大鱼 <58991169+miaowmint@users.noreply.github.com>
2024-01-25 15:04:45 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,24 +13,25 @@ echo '| | | | | | | (_| | (_) \ V  V /| | | | | | | | | | |_ '
 echo '|_| |_| |_|_|\__,_|\___/ \_/\_/ |_| |_| |_|_|_| |_|\__|'
 echo '                                                       '
 
-echo "此脚本在来自 https://www.xuehaiwu.com/palworld-server/ 的脚本的基础上进行修改"    
-sleep 1s
+echo "此脚本在来自 https://www.xuehaiwu.com/palworld-server/ 的脚本的基础上进行修改"  
+sleep 3s  
 echo "感谢腾讯云提供的测试服务器"    
 
 if ! command -v curl &> /dev/null; then
     echo "curl 未安装，正在使用 apt 安装..."
     sudo apt update
     sudo apt install -y curl
-else
-    echo "curl 已安装"
 fi
+
+rm /usr/local/bin/palworld
+rm /usr/local/sh/palworld.sh
 
 mkdir -p /usr/local/sh && curl -o /usr/local/sh/palworld.sh https://raw.githubusercontent.com/miaowmint/palworld/main/palworld.sh
 
 ln -s /usr/local/sh/palworld.sh /usr/local/bin/palworld && chmod +x /usr/local/bin/palworld
 
-echo "后续管理幻兽帕鲁服务端无需重新运行此脚本，只需要在命令行输入 palworld 即可"
+echo -e "后续管理幻兽帕鲁服务端无需重新运行此脚本，只需要在命令行输入\033[32m palworld \033[0m即可"
 
-sleep 3s
+sleep 5s
 
 palworld
